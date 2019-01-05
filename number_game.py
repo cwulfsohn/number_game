3e00e1ff23b3647246cf5a72ff92f66b0ff5b695
Curtis Wulfsohn <curtis@Curtiss-MacBook-Pro.local>
2019-01-05 16:00:39 -0600

    second commit

---
+++ number_game.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'secret'
+import random
+
+@app.route('/')
+def home():
+    try:
+        session['temp'] == 1
+    except:
+        session['guess'] = random.randrange(0,101)
+        session['temp'] = 1
+        session['number_test'] = None
+    return render_template('number.html')
+
+@app.route('/check', methods=['POST'])
+def test():
+    session['number_test'] = int(request.form['number_guess'])
+    return redirect('/')
+
+@app.route('/replay', methods=['POST'])
+def play_again():
+    session.clear()
+    return redirect('/')
+
+app.run(debug=True)
